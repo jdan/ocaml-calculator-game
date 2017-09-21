45bf7f9f9ca2f25bb17803531bc58957f96addd0
Jordan Scales <scalesjordan@gmail.com>
2017-09-20 22:00:08 -0400

    add levels 44, 56, and 70 which use reverse

--- main.ml
+++ main.ml
@@ -9,7 +9,15 @@ let rec combinations options = function
       options
     |> List.flatten
 
-let solve goal init moves buttons =
+type button = string * (int -> int)
+type state = {
+  goal: int;
+  init: int;
+  moves: int;
+  buttons: button list;
+}
+
+let solve { goal; init; moves; buttons } =
   (* Get all button sequences of length `moves` *)
   let all_combinations = combinations buttons moves in
 
@@ -20,6 +28,25 @@ let solve goal init moves buttons =
        (* Reduce the sequence of buttons *)
        List.fold_left (fun acc (_, fn) -> fn acc) init combiation)
     all_combinations
+
+(* Swap `a` digits for `b`s in `n` *)
+let rec swap n a b =
+  if n < 10 then
+    if n = a then b else n
+  else
+    let last_digit = n mod 10 in
+    let rest = swap (n / 10) a b in
+
+    10 * rest +
+    (if last_digit = a then b else last_digit)
+
+let reverse n =
+  let rec inner n acc =
+    if n < 10 then 10 * acc + n
+    else
+      inner (n / 10) (10 * acc + (n mod 10))
+  in
+  inner n 0
 
 let print_solutions solns =
   List.map
@@ -32,51 +59,94 @@ let print_solutions solns =
     solns
   |> List.iter print_endline
 
+let solve_and_print title state =
+  print_endline title;
+  solve state |> print_solutions
+
 let () =
-  print_endline "Level 11";
-  solve 100 99 3 [
-    ("-8",  fun n -> n - 8)  ;
-    ("x11", fun n -> n * 11) ;
-    ("<<",  fun n -> n / 10) ;
-  ]
-  |> print_solutions;
+  solve_and_print "Level 11" {
+    goal = 100 ;
+    init = 99 ;
+    moves = 3 ;
+    buttons = [
+      ("-8",  fun n -> n - 8)  ;
+      ("x11", fun n -> n * 11) ;
+      ("<<",  fun n -> n / 10) ;
+    ]
+  };
 
   print_newline ();
-  print_endline "Level 12";
-  solve 404 0 5 [
-    ("+8",  fun n -> n + 8)  ;
-    ("x10", fun n -> n * 10) ;
-    ("/2",  fun n -> n / 2)  ;
-  ]
-  |> print_solutions;
+  solve_and_print "Level 12" {
+    goal = 404 ;
+    init = 0 ;
+    moves = 5 ;
+    buttons = [
+      ("+8",  fun n -> n + 8)  ;
+      ("x10", fun n -> n * 10) ;
+      ("/2",  fun n -> n / 2)  ;
+    ] ;
+  };
 
   print_newline ();
-  print_endline "Level 24";
-  solve 210 0 5 [
-    ("-5", fun n -> n - 5) ;
-    ("+5", fun n -> n + 5) ;
-    ("5",  fun n -> 5 + n * 10) ;
-    ("2",  fun n -> 2 + n * 10) ;
-  ]
-  |> print_solutions;
-
-  (* Swap `a` digits for `b`s in `n` *)
-  let rec swap n a b =
-    if n < 10 then
-      if n = a then b else n
-    else
-      let last_digit = n mod 10 in
-      let rest = swap (n / 10) a b in
-
-      10 * rest +
-      (if last_digit = a then b else last_digit) in
+  solve_and_print "Level 24" {
+    goal = 210 ;
+    init = 0 ;
+    moves = 5 ;
+    buttons = [
+      ("-5", fun n -> n - 5) ;
+      ("+5", fun n -> n + 5) ;
+      ("5",  fun n -> 5 + n * 10) ;
+      ("2",  fun n -> 2 + n * 10) ;
+    ] ;
+  };
 
   print_newline ();
-  print_endline "Level 30";
-  solve 2321 0 6 [
-    ("1", fun n -> 1 + n * 10) ;
-    ("2", fun n -> 2 + n * 10) ;
-    ("1=>2", fun n -> swap n 1 2) ;
-    ("2=>3", fun n -> swap n 2 3) ;
-  ]
-  |> print_solutions;
+  solve_and_print "Level 30" {
+    goal = 2321 ;
+    init = 0 ;
+    moves = 6 ;
+    buttons = [
+      ("1", fun n -> 1 + n * 10) ;
+      ("2", fun n -> 2 + n * 10) ;
+      ("1=>2", fun n -> swap n 1 2) ;
+      ("2=>3", fun n -> swap n 2 3) ;
+    ] ;
+  };
+
+  print_newline ();
+  solve_and_print "Level 44" {
+    goal = 52 ;
+    init = 44 ;
+    moves = 5 ;
+    buttons = [
+      ("+9", (+) 9) ;
+      ("/2", fun n -> n / 2) ;
+      ("x4", ( * ) 4) ;
+      ("+/-", ( * ) (-1)) ;
+    ] ;
+  };
+
+  print_newline ();
+  solve_and_print "Level 56" {
+    goal = 100 ;
+    init = 1101 ;
+    moves = 4 ;
+    buttons = [
+      ("-1", fun n -> n - 1) ;
+      ("Reverse", reverse) ;
+    ] ;
+  };
+
+  print_newline ();
+  solve_and_print "Level 70" {
+    goal = 81 ;
+    init = 7 ;
+    moves = 5 ;
+    buttons = [
+      ("-9", fun n -> n - 9) ;
+      ("x3", ( * ) 3) ;
+      ("+4", (+) 4) ;
+      ("+/-", ( * ) (-1)) ;
+      ("Reverse", reverse) ;
+    ] ;
+  };
